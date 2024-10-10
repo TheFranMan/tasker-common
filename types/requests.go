d8fa56a420f7963a6863b1a0a5528d27fb6db361
Francis Dolan <francis.dolan@gmail.com>
2024-10-10 16:13:39 +0100

    Added types and vars for requests

--- types/requests.go
+++ types/requests.go
@@ -1,9 +1,61 @@
 package types
 
-type ActionType string
+import (
+	"database/sql"
+	"encoding/json"
+	"time"
+)
 
 var (
+	RequestStatusNew        RequestStatus = 0
+	RequestStatusInProgress RequestStatus = 1
+	RequestStatusCompleted  RequestStatus = 2
+	RequestStatusFailed     RequestStatus = 3
+
+	JobStatusNew        JobStatus = 0
+	JobStatusInProgress JobStatus = 1
+	JobStatusCompleted  JobStatus = 2
+	JobStatusFailed     JobStatus = 3
+	JobStatusRetry      JobStatus = 4
+
 	ActionDelete ActionType = "delete"
 )
 
+type RequestStatus int
+type JobStatus int
+type ActionType string
+
 type Extras map[string]string
+
+type Request struct {
+	Token        string         `db:"token"`
+	RequestToken string         `db:"request_token"`
+	Action       string         `db:"action"`
+	Params       Params         `db:"params"`
+	Extras       sql.NullString `db:"extras"`
+	Steps        Steps          `db:"steps"`
+	Step         int            `db:"step"`
+	Status       int            `db:"status"`
+	Created      time.Time      `db:"created"`
+	Completed    sql.NullTime   `db:"completed"`
+}
+
+type Params struct {
+	ID    int    `json:"id"`
+	Email string `json:"email"`
+}
+
+func (p *Params) Scan(value interface{}) error {
+	if nil == value {
+		p = &Params{}
+		return nil
+	}
+
+	return json.Unmarshal(value.([]byte), p)
+}
+
+type Steps []Step
+type Step struct {
+	Name string   `json:"name"`
+	Jobs []string `json:"jobs"`
+}
